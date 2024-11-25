e5708d5eaaf073c7a437fed84b0635c7a1bb8415
zainmasood22ltsb <154008201+zainmasood22ltsb@users.noreply.github.com>
2024-11-25 20:57:27 +0000

    done

---
+++ app.js
@@ -0,0 +1,18 @@
+// function abc ( ) {
+//     confirm ("hell oworld")
+// }
+// abc ()
+// for (var  i =0 ; i<10  ;i++ ){
+//     abc()
+// }
+// function sum (a,b,c){
+//     return a/b + c
+// }
+// alert(sum(5,4,4));
+// function zoe (a=2,b=3,c=5){
+//     return a+b+c
+// }
+// console.log(zoe())
+
+
+
